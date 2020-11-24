dff64346a585d17c0f5468cac60fcf8e768ec92c
Charles Liu <charlesliu@charlesliusmbp.lan>
2020-11-23 22:54:58 -0800

    added layout

--- Startup_Window.py
+++ Startup_Window.py
@@ -5,3 +5,9 @@ class Startup_Window(object):
         #TODO: create and initialize widgets
 
         #TODO: create layout
+        self.title_frame.grid()
+        self.main_title_label.grid(row=0, columnspan=2)
+        self.content_frame.grid(row=1)
+        self.sub_title_label.grid(row=0)
+        self.linear_regression_button.bind(row=1)
+        self.random_forest.bind(row=2)
