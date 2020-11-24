6b841de3db0865ca43ef968f06a9a60e3bdca628
Stephen Yang <stephen@MacBook-Pro.local>
2020-11-24 11:43:47 -0800

    Updated the buttons and title for the startup window

--- Startup_Window.py
+++ Startup_Window.py
@@ -1,7 +1,21 @@
 from tkinter import *
+from tkinter import font as tkFont
 
 class Startup_Window(object):
     def __init__(self, master, title, size):
         #TODO: create and initialize widgets
+        self.master = master
+        self.title = title
+        self.size = size
+        self.master.title(self.title)
+        self.master.geometry(self.size)
+        self.name = Label(root, text="Predictive Smoking Algorithm", font = helvetica(50, True))
+        self.name.pack()
+        self.items = ["Linear Regression", "Random Forest Regression", "KNN"] #KNN Optional
+        for item in self.items:
+            button = Button(root, text=item, width=25, pady = 50, font = helvetica(40)) #command not implented yet
+            button.pack()
 
         #TODO: create layout
+def helvetica(x, bold=False):
+    return tkFont.Font(family='Helvetica', size=x, weight=(lambda bold: tkFont.BOLD if bold else tkFont.NORMAL) (bold) )
