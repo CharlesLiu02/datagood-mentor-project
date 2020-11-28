5f3eef8267ed5095d801726a827ef03607836865
Charles Liu <liu.charles02@gmail.com>
2020-11-28 15:40:05 -0800

    only show columns that matter, added qualitative data

--- ChecklistBox.py
+++ ChecklistBox.py
@@ -1,7 +1,9 @@
 import tkinter as tk
+from tkinter import *
+from entry_with_placeholder import EntryWithPlaceholder
 
 class ChecklistBox(tk.Frame):
-    def __init__(self, parent, choices, **kwargs):
+    def __init__(self, parent, isChildren, choices, **kwargs):
         tk.Frame.__init__(self, parent, **kwargs)
 
         self.vars = []
@@ -17,6 +19,21 @@ class ChecklistBox(tk.Frame):
             cb.deselect()
             cb.pack(side="top", fill="x", anchor="w")
 
+        def activateCheck():
+            if self.checker.get() == 1:  # whenever checked
+                self.entry.config(state=NORMAL)
+            elif self.checker.get() == 0:  # whenever unchecked
+                self.entry.config(state=DISABLED)
+
+        self.checker = IntVar()
+        if isChildren:
+            self.chk = Checkbutton(self, variable=self.checker, command=activateCheck, text="children: ",
+                                   background=bg, relief="flat", highlightthickness=0)
+            self.chk.pack(side="left")
+            self.entry = EntryWithPlaceholder(self, "Enter # children")
+
+            self.entry.pack(side="left")
+            self.entry.config(state=DISABLED)
 
     def getCheckedItems(self):
         values = []
@@ -24,4 +41,6 @@ class ChecklistBox(tk.Frame):
             value = var.get()
             if value:
                 values.append(value)
+        if self.checker.get() == 1:
+            values.append("children: " + str(self.entry.get()))
         return values
