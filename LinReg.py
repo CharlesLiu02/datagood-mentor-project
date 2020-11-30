0652d12f9efe06b177c56bff04690656a78116e8
Charles Liu <liu.charles02@gmail.com>
2020-11-29 20:34:55 -0800

    clean up imports

--- LinReg.py
+++ LinReg.py
@@ -2,7 +2,6 @@ import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
-from sklearn import preprocessing
 import tkinter as tk
 from tkinter import *
 from tkinter import ttk
